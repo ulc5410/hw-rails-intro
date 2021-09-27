c06a8b2ca7992c2ec1dab4142b67867aa41679e4
EC2 Default User <ec2-user@ip-172-31-4-187.us-east-2.compute.internal>
2021-09-27 20:40:13 +0000

    part3 d

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
     def sortable(column, title=nil)
         title = column.titleize
         if params[:sort]
-            css_class = column == params[:sort]? "hilite bg-warning text-dark":nil
+            css_class = column == params[:sort]? "hilite bg-warning":nil
         else
             if session[:sort]
-                css_class = column == session[:sort]? "hilite bg-warning text-dark":nil
+                css_class = column == session[:sort]? "hilite bg-warning":nil
             end
         end
         link_to title, {:sort => column}, {:class => css_class}
